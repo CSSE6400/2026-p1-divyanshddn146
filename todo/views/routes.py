1a108d19be9666d79629b523564b4ff749e3b93b
Divyansh Agarwal <divyansh.roorkee@gmail.com>
2026-03-23 09:01:53 +0530

    Submit Practical 1 code corrected

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,15 +2,15 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-# This is our "Golden Copy"
+# The 2026 "Golden Copy" from your worksheet
 MOCK_TODO = {
     "id": 1,
     "title": "Watch CSSE6400 Lecture",
     "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
     "completed": True,
-    "deadline_at": "2023-02-27T00:00:00",
-    "created_at": "2023-02-20T00:00:00",
-    "updated_at": "2023-02-20T00:00:00"
+    "deadline_at": "2026-02-27T18:00:00",
+    "created_at": "2026-02-20T14:00:00",
+    "updated_at": "2026-02-20T14:00:00"
 }
 
 @api.route('/health')
@@ -19,25 +19,20 @@ def health():
 
 @api.route('/todos', methods=['GET'])
 def get_todos():
-    # Return a LIST [] because it's a collection
     return jsonify([MOCK_TODO])
+
+@api.route('/todos/<int:todo_id>', methods=['GET'])
+def get_todo(todo_id):
+    return jsonify(MOCK_TODO)
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-    # Return 201 Created and a single OBJECT {}
     return jsonify(MOCK_TODO), 201
 
-@api.route('/todos/<int:id>', methods=['GET'])
-def get_todo(id):
-    # Return a single OBJECT {}
+@api.route('/todos/<int:todo_id>', methods=['PUT'])
+def update_todo(todo_id):
     return jsonify(MOCK_TODO)
 
-@api.route('/todos/<int:id>', methods=['PUT'])
-def update_todo(id):
-    # Return a single OBJECT {}
-    return jsonify(MOCK_TODO)
-
-@api.route('/todos/<int:id>', methods=['DELETE'])
-def delete_todo(id):
-    # Return a single OBJECT {}
+@api.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
     return jsonify(MOCK_TODO)
